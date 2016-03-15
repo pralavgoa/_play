932bab1e8d82c0f5be0ef01e85f842d37f87a465
Pralav Dessai <pralavgoa@gmail.com>
2016-03-15 12:33:35 -0700

    routes for binary view

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -13,17 +13,8 @@ class Application extends Controller {
     Ok(views.html.index("Your new application is ready. Pralav Dessai."))
   }
 
-  def home = Action {
-    // access "default" database
-    DB.withConnection {implicit connection =>
-    // do whatever you need with the connection
-    val selectTableAccessEntries = SQL("SELECT * FROM TABLE_ACCESS")
-    val tableAccessEntries = selectTableAccessEntries().map(row=>
-      row[String]("C_NAME") -> row[String]("C_FULLNAME")
-    ).toList
-    Logger.info(tableAccessEntries.toString)
-    }
-    Ok(views.html.home("Hello world"))
+  def home = Action { 
+    Ok(views.html.home("Hello world"+selectTableAccess))
   }
 
   def d3 = Action {
@@ -32,5 +23,27 @@ class Application extends Controller {
   
   def d3_playground = Action {
     Ok(views.html.d3_playground())
+  }
+
+  def d3_binary = Action {
+    Ok(views.html.d3_binary())
+  }
+
+  def dashboard = Action {
+    Ok(views.html.dashboard())
+  }
+
+  def selectTableAccess =  {
+    var tableAccessEntriesAsString = ""
+    // access "default" database
+    DB.withConnection {implicit connection =>
+    // do whatever you need with the connection
+    val selectTableAccessEntries = SQL("SELECT * FROM TABLE_ACCESS")
+    val tableAccessEntries = selectTableAccessEntries().map(row=>
+      row[String]("C_NAME") -> row[String]("C_FULLNAME")
+    ).toList
+    tableAccessEntriesAsString = tableAccessEntries.toString
+    }
+    tableAccessEntriesAsString
   }
 }
